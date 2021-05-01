923fcf4ee30619c5e433533909c7f3a18dfbb390
ryan.jung <ryan.jung@balancehero.com>
2021-05-02 04:19:50 +0900

    ignore

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/jaemunbro.github.io/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210502.0414';
+const cacheName = 'chirpy-20210502.0419';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
